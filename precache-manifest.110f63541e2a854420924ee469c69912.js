73c0283bc9cc3fd170ecb810741744b69d799847
baciey <baciey@gmail.com>
2020-06-16 23:14:02 +0200

    Updates

---
+++ precache-manifest.110f63541e2a854420924ee469c69912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f352647bcb1291ec696541ece0353dd1",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "81ee13ceda634178639b",
+    "url": "/toDoList/static/css/main.f842c799.chunk.css"
+  },
+  {
+    "revision": "a2471fea3b34108549dd",
+    "url": "/toDoList/static/js/2.e42b893c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoList/static/js/2.e42b893c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ee13ceda634178639b",
+    "url": "/toDoList/static/js/main.c3aa08f6.chunk.js"
+  },
+  {
+    "revision": "3ee9c4e2457ea3cdc03f",
+    "url": "/toDoList/static/js/runtime-main.22df8114.js"
+  }
+]);
